73f7200875c2b6d7232e39896e2a6eae873db256
Alex <li.aa2012@yandex.ru>
2020-07-30 15:14:38 +0300

    day2

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true }));
 app.use('/', require('./routes/auth.routers'));
-app.use(express.static(path.join(__dirname + '/public/css')))
-app.use(express.static(path.join(__dirname + '/public/js')))
-app.use(express.static(path.join(__dirname + '/public/fonts')))
-app.use(express.static(path.join(__dirname + '/public/icons')))
-
+app.use(express.static(path.join(__dirname + '/public/css')));
+app.use(express.static(path.join(__dirname + '/public/js')));
+app.use(express.static(path.join(__dirname + '/public/fonts')));
+app.use(express.static(path.join(__dirname + '/public/icons')));
+app.use(express.static(path.join(__dirname + '/public/img')));
 
 module.exports = app;
 
--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,24 @@
-let header__box = document.querySelector('.header__box');
-let header__checkbox = document.querySelector('.header__checkbox');
-header__box.addEventListener('click', () => {
-    if(header__box.classList.contains('checked')){
-        header__box.classList.remove('checked');
-    }else{
-        header__box.classList.add('checked');
-    }
-});
+let header__box = document.querySelectorAll('.header__box');
+let header__login = document.querySelectorAll('.header__login');
+let header__signup =document.querySelector('.header__signup');
+
+header__login.forEach((login) => {
+    login.style.color = "red";
+})
+
+header__box.forEach((box) => {
+    box.addEventListener('click', (event) => {
+        console.log(event.classList)
+        if(event.target.classList.contains('checked')){
+            event.target.classList.remove('checked');
+            event.target.style.left = -1 + 'px';
+            header__signup.style.color = 'white';
+            header__login.style.color = 'red';
+        }else{
+            event.target.classList.add('checked');
+            event.target.style.left = 17 + 'px';
+            header__signup.style.color = "red";
+            header__login.style.color = 'white';
+        }
+    });
+})
